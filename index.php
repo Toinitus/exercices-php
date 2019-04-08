fa0dbe122f92fc351b25b29e071ea1b08d79ecb8
Toinitus <antoinego96@orange.fr>
2019-04-08 11:07:49 +0200

    status corriger

--- index.php
+++ index.php
@@ -9,17 +9,17 @@
 	$TabDep = array("02"=>"Aisne", "59"=>"Nord", "60"=>"Oise", "62"=>"Pas-De-Calais", "80"=>"Somme");
 	echo '<br />'.$TabDep['59'];
 
-	 echo '<br />'.$TabDep[51]="Marne";
+	echo '<br />'.$TabDep[51]="Marne";
 		
 	
 	foreach ($TabMois as  $value) {
 		echo '<br />'.$value;
+
+		//for ($i=0; $i< count ($mois) ; $i++) { echo "br/>"; echo $mois[$i];}
 	}
 	foreach ($TabDep as $key => $value) {
 	echo  '<br />' ."<p> Le numero: "  .$key. " est le departement " .$value. "<p>";
 	}
-
-	
 		
 	
 ?>
